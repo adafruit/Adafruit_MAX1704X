aca80448517eb117c958826f08c204ba1ac50e45
Doxygen CI <ci-arduino@invalid>
2024-03-25 23:31:58 +0000

    Deploy docs to GitHub Pages from commit 4953d76cc9
    Commit: 4953d76cc98fc3f2b072b477d80519b32f1f59dc
    GitHub Actions run: 8428369257

--- html/search/all_6.js
+++ html/search/all_6.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['i2c_5fdev',['i2c_dev',['../class_adafruit___m_a_x17048.html#a6330202fb2511ccfc16180fc28ddb678',1,'Adafruit_MAX17048']]],
   ['isactivealert',['isActiveAlert',['../class_adafruit___m_a_x17048.html#a079a6cff8d4c1f844587e7f8e24ab67b',1,'Adafruit_MAX17048']]],
+  ['isdeviceready',['isDeviceReady',['../class_adafruit___m_a_x17048.html#a76a73e7784b05b0e6f4e683e401b627a',1,'Adafruit_MAX17048']]],
   ['ishibernating',['isHibernating',['../class_adafruit___m_a_x17048.html#ac3e7abe792e6cd7f3a402cd060ff24b0',1,'Adafruit_MAX17048']]]
 ];
--- html/search/functions_6.js
+++ html/search/functions_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['isactivealert',['isActiveAlert',['../class_adafruit___m_a_x17048.html#a079a6cff8d4c1f844587e7f8e24ab67b',1,'Adafruit_MAX17048']]],
+  ['isdeviceready',['isDeviceReady',['../class_adafruit___m_a_x17048.html#a76a73e7784b05b0e6f4e683e401b627a',1,'Adafruit_MAX17048']]],
   ['ishibernating',['isHibernating',['../class_adafruit___m_a_x17048.html#ac3e7abe792e6cd7f3a402cd060ff24b0',1,'Adafruit_MAX17048']]]
 ];
